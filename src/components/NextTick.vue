053081a8c01fad06f7f0b1d09312b0605d88df1c
Celine.Cui <linn21@163.com>
2022-04-06 18:32:53 +0800

    vdom-diff

---
+++ src/components/NextTick.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h2>$nextTick</h2>
+      <!-- <div id="id1" ref="msg">{{message}}</div> -->
+      <div @click="changeMessage" ref="msg">{{message}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'NextTick',
+    data(){
+        return{
+            message:'未更新'
+        }
+    },
+    methods:{
+        changeMessage(){
+            let that = this;
+            that.message = '已更新'
+            console.log('dom1',this.$refs.msg.innerText) //未更新
+            that.$nextTick(function(){
+                console.log('dom2',this.$refs.msg.innerText) //已更新
+            })
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
