77b0e8ba837e090596677ca0e63880789a230d26
Celine.Cui <linn21@163.com>
2022-05-17 17:27:37 +0800

    自定义组件使用v-model

---
+++ src/components/child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <button @click="change">改变</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'child',
+  props:['msg'],
+  model:{
+    prop:'msg',
+    event:'changeMsg'
+  },
+  methods:{
+    change(){
+      this.$emit('changeMsg','我被子组件改变了')
+    }
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/parent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <child v-model="test"></child>
+  </div>
+</template>
+
+<script>
+import child from './child.vue'
+export default {
+  components:{ child },
+  name:'parent',
+  data(){
+    return {
+      test:'abc'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
